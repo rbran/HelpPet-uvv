3396f58bc3ebdf57ed90ff13ca68b93868d252c7
rbran <433c8ad8@opayq.com>
2016-04-06 22:08:34 +0000

    Adicionado na tela inicial do Frontend uma lista de pets pertencentes ao usuario.

--- static/js/controller/home.js
+++ static/js/controller/home.js
@@ -1,10 +1,47 @@
-app.controller('HomeController', function($scope, $location, store)  {
-        if(!$scope.isLoged()){
-            $location.path('login');
-        }
+app.factory('manterPets', function($http) {
+    var url = "/dynamic/pets.php";
+    
+    var getPetsPorDono = function(idDono) {
+        var enviar = {idDono: idDono};
         
-        $scope.logout = function() {
-            store.remove('jwt');
-            $location.path('login');
+        return $http.post(url, enviar).then(
+            function sucesso(respostaServidor) {
+                return respostaServidor.data;
+            },
+            function erro(respostaServidor) {
+                return {resposta:"erro", mensagem: "Erro ao se comunicar com a servidor"};
+            });
+    }
+    
+    return {
+        getPetsPorDono: getPetsPorDono
+    };
+});
+
+app.controller('HomeController', function($scope, $location, store, jwtHelper, manterPets)  {
+    if(!$scope.isLoged()){
+        $location.path('login');
+        return;
+    }
+    
+    $scope.dataHome = {};
+    
+    var jwt = jwtHelper.decodeToken(store.get('jwt'));
+    
+    $scope.dataHome.usuario = jwt.data.usuario;
+    
+    var resposta = manterPets.getPetsPorDono(jwt.data.id);
+    
+    resposta.then(function(data) {
+        if(data.resposta == "sucesso"){
+            $scope.dataHome.pets = data.idDono;
+        }else{
+            alert("Erro ao receber dados do servidor");
         }
     });
+    
+    $scope.logout = function() {
+        store.remove('jwt');
+        $location.path('login');
+    }
+});
