ef436d6925b92c2df34e8506555e84acda9077dc
rbran <433c8ad8@opayq.com>
2016-05-10 21:47:08 +0000

    Adiciona ação aos botoes mostra detalhes em Ver Pets {Perdidos,Adoção}

--- static/js/controller/pesquisa-adocao.js
+++ static/js/controller/pesquisa-adocao.js
@@ -1,12 +1,13 @@
 app.controller('PesquisaAdocaoController', function($scope, $location, petsService)  {
     $scope.dataPesquisaAdocao = {};
     $scope.dataPesquisaAdocao.pets = [];
+    $scope.dataPesquisaAdocao.petDetalhe = null;
     
+    $scope.mostraDetalhe = function(index) {
+        $scope.dataPesquisaAdocao.petDetalhe = $scope.dataPesquisaAdocao.pets[index];
+    }
     
-    
-    var respostaPets = petsService.getPetsAdocao();
-    
-    respostaPets.then(function(data) {
+    petsService.getPetsAdocao().then(function(data) {
         if(data.resposta == "sucesso") {
             $scope.dataPesquisaAdocao.pets = data.consultarAdocao;
         }else{
